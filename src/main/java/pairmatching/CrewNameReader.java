eb316327435e705644536292be7d4ebfdba941e2
drunkenhw <drunkenhw@gmail.com>
2022-12-08 14:18:41 +0900

    refactor(CrewNameReader): 크루의 코스에 따라 파일을 읽도록 변경

--- src/main/java/pairmatching/CrewNameReader.java
+++ src/main/java/pairmatching/CrewNameReader.java
@@ -6,11 +6,15 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import pairmatching.model.Course;
 
 public class CrewNameReader {
-    public static List<String> getCrewNames(String filePath) {
+    private static final String BACKEND_CREW_FILE_PATH = "src/main/resources/backend-crew.md";
+    private static final String FRONTEND_CREW_FILE_PATH = "src/main/resources/frontend-crew.md";
+
+    public static List<String> getCrewNames(Course course) {
         List<String> crews = new ArrayList<>();
-        File file = new File(filePath);
+        File file = getFileByCourse(course);
         try (BufferedReader br = new BufferedReader(new FileReader(file))) {
             br.lines()
                     .forEach(crews::add);
@@ -18,5 +22,12 @@ public class CrewNameReader {
         } catch (IOException e) {
             throw new IllegalArgumentException();
         }
+    }
+
+    private static File getFileByCourse(Course course) {
+        if (course == Course.BACKEND) {
+            return new File(BACKEND_CREW_FILE_PATH);
+        }
+        return new File(FRONTEND_CREW_FILE_PATH);
     }
 }
--- src/test/java/pairmatching/CrewNameReaderTest.java
+++ src/test/java/pairmatching/CrewNameReaderTest.java
@@ -6,14 +6,15 @@ import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import pairmatching.model.Course;
 
 class CrewNameReaderTest {
 
     @DisplayName("파일에서 크루의 이름을 저장한다.")
     @ParameterizedTest
-    @CsvSource({"src/main/resources/backend-crew.md", "src/main/resources/frontend-crew.md"})
-    void backendTest(String filePath) {
-        List<String> crewNames = CrewNameReader.getCrewNames(filePath);
+    @CsvSource({"FRONTEND", "BACKEND"})
+    void backendTest(Course course) {
+        List<String> crewNames = CrewNameReader.getCrewNames(course);
 
         assertThat(crewNames.size()).isNotZero();
     }
