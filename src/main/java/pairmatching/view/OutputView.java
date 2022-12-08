a60093ff0391e8ff5178b1fb70eeb33b674953d0
ksh-beginner <ohk9134@naver.com>
2022-12-08 13:20:37 +0900

    feat(OutputView) : 기능 커맨드 출력 기능 구현

--- src/main/java/pairmatching/view/OutputView.java
+++ src/main/java/pairmatching/view/OutputView.java
@@ -3,9 +3,21 @@ package pairmatching.view;
 public class OutputView {
 
     private static final String FUNCTION_CHOICE_MESSAGE = "기능을 선택하세요.";
+    private static final String PAIR_MATCHING_COMMAND_MESSAGE = "1. 페어 매칭";
+    private static final String PAIR_FIND_COMMAND_MESSAGE = "2. 페어 조회";
+    private static final String PAIR_INITIALIZE_COMMAND_MESSAGE = "3. 페어 초기화";
+    private static final String END_COMMAND_MESSAGE = "Q. 종료";
+
 
     public void printFunctionChoiceMessage() {
         printMessage(FUNCTION_CHOICE_MESSAGE);
+    }
+
+    public void printFunctionCommand() {
+        printMessage(PAIR_MATCHING_COMMAND_MESSAGE);
+        printMessage(PAIR_FIND_COMMAND_MESSAGE);
+        printMessage(PAIR_INITIALIZE_COMMAND_MESSAGE);
+        printMessage(END_COMMAND_MESSAGE);
     }
 
     private void printMessage(String message) {
