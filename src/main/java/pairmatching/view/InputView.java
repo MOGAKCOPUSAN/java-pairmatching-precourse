19842c242166e36da41802daef8a6de5d88d87c3
kim0914 <kosa0914@naver.com>
2022-12-08 15:40:24 +0900

    feat(InputView): 사용자의 기능 선택 메서드 추가

--- src/main/java/pairmatching/view/InputView.java
+++ src/main/java/pairmatching/view/InputView.java
@@ -1,5 +1,13 @@
 package pairmatching.view;
 
+import camp.nextstep.edu.missionutils.Console;
+import pairmatching.domain.Course;
+import pairmatching.domain.Level;
+import pairmatching.util.Parser;
+import pairmatching.validator.InputValidator;
+
+import java.util.List;
+
 public class InputView {
 
     public static final String INPUT_CHOICE_MESSAGE = "기능을 선택하세요.";
@@ -16,4 +24,24 @@ public class InputView {
     public static final String COLON_MESSAGE = ": ";
     public static final String COURSE_LEVEL_MISSION_INPUT_MESSAGE = "과정, 레벨, 미션을 선택하세요.";
     public static final String COURSE_LEVEL_MISSION_EXAMPLE_MESSAGE = "ex) 백엔드, 레벨1, 자동차경주";
+
+    public String readChoice() {
+        printChoiceInputMessage();
+
+        String choice = Console.readLine();
+        InputValidator.checkChoice(choice);
+        return choice;
+    }
+
+    private void printChoiceInputMessage() {
+        printMessage(INPUT_CHOICE_MESSAGE);
+        printMessage(FIRST_CHOICE_MESSAGE);
+        printMessage(SECOND_CHOICE_MESSAGE);
+        printMessage(THIRD_CHOICE_MESSAGE);
+        printMessage(QUIT_CHOICE_MESSAGE);
+    }
+
+    private void printMessage(String message) {
+        System.out.println(message);
+    }
 }
