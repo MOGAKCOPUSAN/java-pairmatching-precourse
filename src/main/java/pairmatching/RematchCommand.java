11b7672d5126f27af27366b1a8c318e774b236b2
ksh-beginner <ohk9134@naver.com>
2022-12-09 01:53:17 +0900

    feat(RematchCommand) : 재매칭 시 네, 아니오가 아니면 예외 처리 구현

---
+++ src/main/java/pairmatching/RematchCommand.java
@@ -0,0 +1,22 @@
+package pairmatching;
+
+import java.util.Arrays;
+
+public enum RematchCommand {
+    YES("네"),
+    NO("아니오");
+
+    private String key;
+
+    RematchCommand(String key) {
+        this.key = key;
+    }
+
+    public static RematchCommand getRematchCommand(String rematchCommand) {
+        return Arrays.stream(values())
+                .filter(value -> value.key.equals(rematchCommand))
+                .findAny()
+                .orElseThrow(() -> new IllegalArgumentException(ErrorConstants.ERROR_PREFIX
+                        + "재매치 여부를 제대로 입력해주세요."));
+    }
+}
