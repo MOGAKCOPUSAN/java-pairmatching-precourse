69d6a32dc491c6bfe69fb25befeffb0a1d4b0c69
ksh-beginner <ohk9134@naver.com>
2022-12-08 17:26:10 +0900

    feat(Level) : 없는 레벨 입력 시 예외 처리 구현

--- src/main/java/pairmatching/Level.java
+++ src/main/java/pairmatching/Level.java
@@ -1,5 +1,7 @@
 package pairmatching;
 
+import java.util.Arrays;
+
 public enum Level {
     LEVEL1("레벨1"),
     LEVEL2("레벨2"),
@@ -11,5 +13,13 @@ public enum Level {
 
     Level(String name) {
         this.name = name;
+    }
+
+    public static Level getCourse(String levelName) {
+        return Arrays.stream(values())
+                .filter(value -> value.name.equals(levelName))
+                .findAny()
+                .orElseThrow(() ->
+                        new IllegalArgumentException(ErrorConstants.ERROR_PREFIX + "정확한 레벨을 입력해주세요."));
     }
 }
