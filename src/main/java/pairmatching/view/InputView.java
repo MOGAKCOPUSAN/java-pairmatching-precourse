84af0b9965a968308c3289a41bbe6bdeb689ae36
kim0914 <kosa0914@naver.com>
2022-12-08 15:39:18 +0900

    feat(InputView): 입력 시 사용하는 문자열 상수 정의

---
+++ src/main/java/pairmatching/view/InputView.java
@@ -0,0 +1,19 @@
+package pairmatching.view;
+
+public class InputView {
+
+    public static final String INPUT_CHOICE_MESSAGE = "기능을 선택하세요.";
+    public static final String FIRST_CHOICE_MESSAGE = "1. 페어 매칭";
+    public static final String SECOND_CHOICE_MESSAGE = "2. 페어 조회";
+    public static final String THIRD_CHOICE_MESSAGE = "3. 페어 초기화";
+    public static final String QUIT_CHOICE_MESSAGE = "Q. 종료";
+
+    public static final String LINE_SEPARATOR_MESSAGE = "#############################################";
+    public static final String COURSE_CHOICE_MESSAGE = "과정: ";
+    public static final String MISSION_CHOICE_MESSAGE = "미션: ";
+    public static final String DELIMITER = " | ";
+    public static final String MISSION_HEADER_MESSAGE = " -";
+    public static final String COLON_MESSAGE = ": ";
+    public static final String COURSE_LEVEL_MISSION_INPUT_MESSAGE = "과정, 레벨, 미션을 선택하세요.";
+    public static final String COURSE_LEVEL_MISSION_EXAMPLE_MESSAGE = "ex) 백엔드, 레벨1, 자동차경주";
+}
