15dcc8ccaac6c0135444bbd14c98b238c1b9d2b5
ksh-beginner <ohk9134@naver.com>
2022-12-08 17:23:43 +0900

    feat(InputView) : 과정, 레벨, 미션 입력 기능 구현

--- src/main/java/pairmatching/view/InputView.java
+++ src/main/java/pairmatching/view/InputView.java
@@ -3,10 +3,22 @@ package pairmatching.view;
 import camp.nextstep.edu.missionutils.Console;
 import pairmatching.FunctionCommand;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class InputView {
+
+    private static final String BLANK = " ";
+    private static final String PROGRAM_SPLIT_DELIMITER = ",";
 
     public FunctionCommand inputFunctionCommand() {
         String functionCommand = Console.readLine();
         return FunctionCommand.getCommand(functionCommand);
+    }
+
+    public List<String> inputChoiceProgram() {
+        String program = Console.readLine();
+        String removeBlankProgram = program.replace(BLANK, "");
+        return Arrays.asList(removeBlankProgram.split(PROGRAM_SPLIT_DELIMITER));
     }
 }
