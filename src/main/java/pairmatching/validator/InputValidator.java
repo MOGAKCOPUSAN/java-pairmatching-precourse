e96ef24becee00697146e7357ac2a9ad1cae84a8
kim0914 <kosa0914@naver.com>
2022-12-08 17:50:29 +0900

    feat(InputValidator): 재매칭 입력 검증 메서드 추가

--- src/main/java/pairmatching/validator/InputValidator.java
+++ src/main/java/pairmatching/validator/InputValidator.java
@@ -6,9 +6,16 @@ import java.util.List;
 public class InputValidator {
 
     public static final List<String> choices = Arrays.asList("1", "2", "3", "Q");
+    public static final List<String> rematchings = Arrays.asList("네", "아니오");
 
     public static void checkChoice(String choice) {
         if (!choices.contains(choice)) {
+            throw new IllegalArgumentException("입력을 잘못하였습니다. 확인해주세요");
+        }
+    }
+
+    public static void checkRematching(String rematching) {
+        if (!rematchings.contains(rematching)) {
             throw new IllegalArgumentException("입력을 잘못하였습니다. 확인해주세요");
         }
     }
