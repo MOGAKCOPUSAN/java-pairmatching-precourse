89d1af453c7b785a9c4a41793b8af2f106346a93
ksh-beginner <ohk9134@naver.com>
2022-12-09 00:30:20 +0900

    refactor(CrewFileReader) : 예외 던지지않고 바로 처리하도록 try-catch 리팩토링

--- src/main/java/pairmatching/CrewFileReader.java
+++ src/main/java/pairmatching/CrewFileReader.java
@@ -11,15 +11,23 @@ import java.util.List;
 
 public class CrewFileReader {
 
-    public static List<String> backendCrewNameFileRead() throws IOException {
+    public static List<String> backendCrewNameFileRead() {
         URL backendCrewsFileUrl = CrewFileReader.class.getClassLoader().getResource("backend-crew.md");
         Path backendCrewsFilePath = new File(backendCrewsFileUrl.getPath()).toPath();
-        return Files.readAllLines(backendCrewsFilePath);
+        try {
+            return Files.readAllLines(backendCrewsFilePath);
+        } catch (IOException e) {
+            throw new RuntimeException(ErrorConstants.ERROR_PREFIX + "백엔드 크루의 파일을 읽어올 수 없습니다.");
+        }
     }
 
-    public static List<String> frontendCrewNameFileRead() throws IOException {
+    public static List<String> frontendCrewNameFileRead() {
         URL frontendCrewsFileUrl = CrewFileReader.class.getClassLoader().getResource("frontend-crew.md");
         Path backendCrewsFilePath = new File(frontendCrewsFileUrl.getPath()).toPath();
-        return Files.readAllLines(backendCrewsFilePath);
+        try {
+            return Files.readAllLines(backendCrewsFilePath);
+        } catch (IOException e) {
+            throw new RuntimeException(ErrorConstants.ERROR_PREFIX + "프론트엔드 크루의 파일을 읽어올 수 없습니다.");
+        }
     }
 }
