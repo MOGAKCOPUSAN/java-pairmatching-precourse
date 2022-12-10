73b39e20231416a13cd5c613b4413d89e93674e5
ksh-beginner <ohk9134@naver.com>
2022-12-10 15:19:34 +0900

    feat(Crew) : 레벨별 페어 크루 목록 관련 기능 구현

--- src/main/java/pairmatching/domain/Crew.java
+++ src/main/java/pairmatching/domain/Crew.java
@@ -1,18 +1,47 @@
 package pairmatching.domain;
 
-import pairmatching.Course;
+import pairmatching.domain.program.Course;
+import pairmatching.domain.program.Level;
+
+import java.util.*;
 
 public class Crew {
 
     private Course course;
     private String name;
+    private final Map<Level, List<Crew>> matchedCrewsByLevel = new HashMap<>();
 
     public Crew(Course course, String name) {
         this.course = course;
         this.name = name;
+        initMatchedCrewsByLevel();
+    }
+
+    private void initMatchedCrewsByLevel() {
+        List<Crew> initCrews = new ArrayList<>(Collections.emptyList());
+        matchedCrewsByLevel.put(Level.LEVEL1, initCrews);
+        matchedCrewsByLevel.put(Level.LEVEL2, initCrews);
+        matchedCrewsByLevel.put(Level.LEVEL3, initCrews);
+        matchedCrewsByLevel.put(Level.LEVEL4, initCrews);
+        matchedCrewsByLevel.put(Level.LEVEL5, initCrews);
     }
 
     public String getName() {
         return name;
+    }
+
+    public void putMatchedCrew(Level level, Crew crewToAdd) {
+        List<Crew> matchedCrews = matchedCrewsByLevel.get(level);
+        matchedCrews.add(crewToAdd);
+        matchedCrewsByLevel.put(level, matchedCrews);
+    }
+
+    public void initializeMatchedCrewsByLevel() {
+        matchedCrewsByLevel.clear();
+    }
+
+    public boolean isAlreadyMatchingCrewByLevel(Crew crew, Level level) {
+        List<Crew> matchedCrews = matchedCrewsByLevel.get(level);
+        return matchedCrews.contains(crew);
     }
 }
