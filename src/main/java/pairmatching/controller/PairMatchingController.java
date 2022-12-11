897161f8ec954cd2dfd414a585a7e13810b395e0
kim0914 <kosa0914@naver.com>
2022-12-11 21:36:56 +0900

    feat(PairMatchingController): 컨트롤러 상수 및 인스턴스 정의

---
+++ src/main/java/pairmatching/controller/PairMatchingController.java
@@ -0,0 +1,25 @@
+package pairmatching.controller;
+
+import pairmatching.domain.*;
+import pairmatching.service.PairMatchingService;
+import pairmatching.util.FileProcessor;
+import pairmatching.util.Log;
+import pairmatching.view.InputView;
+import pairmatching.view.OutputView;
+
+import java.util.List;
+import java.util.Set;
+
+public class PairMatchingController {
+
+    public static final String FILE_PATH = "src/main/resources/";
+    public static final String BACKEND_FILE = "backend-crew.md";
+    public static final String FRONTEND_FILE = "frontend-crew.md";
+    public static final String PAIR_MATCHING = "1";
+    public static final String PAIR_SEARCHING = "2";
+    public static final String PAIR_INITIALIZE = "3";
+    public static final String NO_REMATCHING = "아니오";
+
+    private final InputView inputView = new InputView();
+    private final OutputView outputView = new OutputView();
+}
