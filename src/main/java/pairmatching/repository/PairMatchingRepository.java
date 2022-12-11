5c844ce26896e05969a3c2c454944845c06cc7dd
kim0914 <kosa0914@naver.com>
2022-12-11 21:35:13 +0900

    refactor(PairMatchingRepository): 페어 매칭 결과를 저장하는 key List -> Condition 변경

--- src/main/java/pairmatching/repository/PairMatchingRepository.java
+++ src/main/java/pairmatching/repository/PairMatchingRepository.java
@@ -4,21 +4,20 @@ import pairmatching.domain.Condition;
 import pairmatching.domain.Pair;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 public class PairMatchingRepository {
 
-    private final Map<List<String>, Set<Pair>> matchingResults = new HashMap<>();
+    private final Map<Condition, Set<Pair>> matchingResults = new HashMap<>();
 
-    public void add(List<String> condition, Set<Pair> pairs) {
+    public void add(Condition condition, Set<Pair> pairs) {
         if (!matchingResults.containsKey(condition)) {
             matchingResults.put(condition, pairs);
         }
     }
 
-    public Set<Pair> findByCondition(List<String> condition) {
+    public Set<Pair> findByCondition(Condition condition) {
         if (!matchingResults.containsKey(condition)) {
             throw new IllegalArgumentException("해당 페어 매칭 결과가 존재하지 않습니다.");
         }
